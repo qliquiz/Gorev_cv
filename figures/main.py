71fddd1b653b4f3cd8f2512af749a30fce9f5ab2
qliquiz <artgor005@gmail.com>
2024-03-20 16:59:22 +0800

    wires

--- figures/main.py
+++ figures/main.py
@@ -7,12 +7,38 @@ data = np.load('figures/ps.npy.txt')
 
 labeled = label(data)
 
-type1 = type2 = type3 = type4 = 0
+count_of_struct1 = count_of_struct2 = count_of_struct3 = count_of_struct4 = 0
 
-for label in range(1, labeled.max() + 1):
-    sum+=1
+struct1 = np.array([[1, 1, 1],
+                    [1, 0, 1]])
 
-print(labeled.max())
+struct2 = np.array([[1, 0, 1],
+                    [1, 1, 1]])
 
+struct3 = np.array([[1, 1],
+                    [1, 0],
+                    [1, 1]])
+
+struct4 = np.array([[1, 1],
+                    [0, 1],
+                    [1, 1]])
+
+# for label in range(1, labeled.max() + 1):
+#     if label == struct1:
+#         count_of_struct1 += 1
+#     elif label == struct2:
+#         count_of_struct2 += 1
+#     elif label == struct3:
+#         count_of_struct3 += 1
+#     elif label == struct4:
+#         count_of_struct4 += 1
+
+# print(labeled.max())
+# print(count_of_struct1)
+# print(count_of_struct2)
+# print(count_of_struct3)
+# print(count_of_struct4)
+
+print(struct1 in labeled)
 plt.imshow(labeled)
 plt.show()
--- wires/main.py
+++ wires/main.py
@@ -1,94 +1,30 @@
-from skimage.measure import label
-from skimage.morphology import binary_closing, binary_dilation, binary_opening, binary_erosion
+from skimage.morphology import binary_erosion
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-struct = np.ones((3, 3))
+struct = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
 
-def erosion(arr, struct = struct):
-    result = np.zeros_like(arr)
-    for i in range(1, arr.shape[0] - 1):
-        for j in range(1, arr.shape[1] - 1):
-            sub = arr[i-1:i+2, j-1:j+2]
-            if np.all(sub == struct):
-                result[i, j] = 1
-    return result
 
-# Задача 1
-""" data = np.load('wires/wires1.npy.txt')
-plt.figure()
-plt.imshow(data)
-labeled = label(erosion(data))
-count = 0
-for label in range(1, labeled.max() + 1):
-    plt.figure()
-    plt.imshow(labeled == label)
-    count += 1
-print('Всего проводов', count)
-plt.show() """
+def checking(image):
+    number = 1
+    for y in range(image.shape[0]):
+        count = 0
+        flag = False
+        for x in range(image.shape[1] - 1):
+            if image[y, x] != 0 and image[y, x + 1] == 0:
+                count += 1
+            if image[y, x] != 0 and image[y + 1, x] == 0:
+                flag = True
+        if flag:
+            if count == 0:
+                print(f'Провод {number} цел.')
+            else:
+                print(f'Провод {number} порван на {count + 1} кусков.')
+            number += 1
 
-# Задача 2
-""" data = np.load('wires/wires2.npy.txt')
-plt.figure()
-plt.imshow(data)
-labeled = label(erosion(data))
-count = 0
-for label in range(1, labeled.max() + 1):
-    plt.figure()
-    plt.imshow(labeled == label)
-    count += 1
-print('Всего проводов', count)
-plt.show() """
 
-# Задача 3
-""" data = np.load('wires/wires3.npy.txt')
-plt.figure()
-plt.imshow(data)
-labeled = label(erosion(data))
-count = 0
-for label in range(1, labeled.max() + 1):
-    plt.figure()
-    plt.imshow(labeled == label)
-    count += 1
-print('Всего проводов:', count)
-plt.show() """
-
-# Задача 4
-""" data = np.load('wires/wires4.npy.txt')
-plt.figure()
-plt.imshow(data)
-labeled = label(erosion(data))
-count = 0
-for label in range(1, labeled.max() + 1):
-    plt.figure()
-    plt.imshow(labeled == label)
-    count += 1
-print('Всего проводов:', count)
-plt.show() """
-
-# Задача 5
-""" data = np.load('wires/wires5.npy.txt')
-plt.figure()
-plt.imshow(data)
-labeled = label(erosion(data))
-count = 0
-for label in range(1, labeled.max() + 1):
-    plt.figure()
-    plt.imshow(labeled == label)
-    count += 1
-print('Всего проводов:', count)
-plt.show() """
-
-# Задача 6
-""" data = np.load('wires/wires6.npy.txt')
-plt.figure()
-plt.imshow(data)
-labeled = label(erosion(data))
-count = 0
-for label in range(1, labeled.max() + 1):
-    plt.figure()
-    plt.imshow(labeled == label)
-    count += 1
-print('Всего проводов:', count)
-plt.show() """
+image = np.load('wires/wires5.npy.txt')
+checking(binary_erosion(image, struct))
+plt.imshow(image)
+plt.show()
