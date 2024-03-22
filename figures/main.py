e69fc3175a0a9b8028420859dd7ac33f0311c6f5
qliquiz <artgor005@gmail.com>
2024-03-22 22:03:44 +0800

    figures

--- figures/main.py
+++ figures/main.py
@@ -1,44 +1,72 @@
 import numpy as np
-from skimage.measure import label
 import matplotlib.pyplot as plt
+from skimage.measure import label
+from scipy.ndimage import morphology
 
 
-data = np.load('figures/ps.npy.txt')
+data = np.load("figures/ps.npy.txt")
 
 labeled = label(data)
 
-count_of_struct1 = count_of_struct2 = count_of_struct3 = count_of_struct4 = 0
+mask1 = np.array([
+        [1,1,1,1],
+        [1,1,1,1],
+        [1,1,0,0],
+        [1,1,0,0],
+        [1,1,1,1],
+        [1,1,1,1]])
 
-struct1 = np.array([[1, 1, 1],
-                    [1, 0, 1]])
+mask2 = np.array([
+        [1,1,1,1],
+        [1,1,1,1],
+        [0,0,1,1],
+        [0,0,1,1],
+        [1,1,1,1],
+        [1,1,1,1]])
 
-struct2 = np.array([[1, 0, 1],
-                    [1, 1, 1]])
+mask3 =  np.array([
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1]])
 
-struct3 = np.array([[1, 1],
-                    [1, 0],
-                    [1, 1]])
+mask4 = np.array([
+        [1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1]])
 
-struct4 = np.array([[1, 1],
-                    [0, 1],
-                    [1, 1]])
+mask5 = np.array([
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1]])
 
-# for label in range(1, labeled.max() + 1):
-#     if label == struct1:
-#         count_of_struct1 += 1
-#     elif label == struct2:
-#         count_of_struct2 += 1
-#     elif label == struct3:
-#         count_of_struct3 += 1
-#     elif label == struct4:
-#         count_of_struct4 += 1
+erosed1 = morphology.binary_erosion(data, mask1)
+dilation = morphology.binary_dilation(erosed1, mask1)
+data -= dilation
+print(label(dilation).max())
 
-# print(labeled.max())
-# print(count_of_struct1)
-# print(count_of_struct2)
-# print(count_of_struct3)
-# print(count_of_struct4)
+erosed2 = morphology.binary_erosion(data, mask2)
+dilation2 = morphology.binary_dilation(erosed2, mask2)
+data -= dilation2
+print(label(dilation2).max())
 
-print(struct1 in labeled)
+erosed3 = morphology.binary_erosion(data, mask3)
+dilation3 = morphology.binary_dilation(erosed3, mask3)
+data -= dilation3
+print(label(dilation3).max())
+
+erosed4 = morphology.binary_erosion(data, mask4)
+dilation4 = morphology.binary_dilation(erosed4, mask4)
+data -= dilation4
+print(label(dilation4).max())
+
+erosed5 = morphology.binary_erosion(data, mask5)
+dilation5 = morphology.binary_dilation(erosed5, mask5)
+data -= dilation5
+print(label(dilation5).max())
+
+plt.title(f"Всего {labeled.max()}")
 plt.imshow(labeled)
 plt.show()
